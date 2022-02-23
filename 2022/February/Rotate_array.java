7ebe1d1e3810905bf44cce44b186090cfe4e7c5f
kishore99 <bhashyamyk@gmail.com>
2022-02-23 10:36:27 +0530

    array_rotation by k steps right

---
+++ 2022/February/Rotate_array.java
@@ -0,0 +1,26 @@
+//PS::https://leetcode.com/problems/rotate-array/submissions/
+//23-02-2022
+//Java
+
+class Solution {
+    public void reverse(int[] arr, int left, int right){
+        while(left < right)
+		{  // swap till the array is reversed
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public void rotate(int[] nums, int k) {
+
+        k = k % nums.length;  // This will be highly effiecient when  k > nums.length
+
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+}
