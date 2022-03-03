fb7c410c5641c972873bd9c77cdee38d187e6fda
kishore99 <bhashyamyk@gmail.com>
2022-03-03 09:55:49 +0530

    modified

--- 2022/February/Rotate_array.java
+++ 2022/February/Rotate_array.java
@@ -10,7 +10,7 @@ class Solution {
             arr[left] = arr[right];
             arr[right] = temp;
 
-            left++;
+            left++;//increment by 1
             right--;
         }
     }
