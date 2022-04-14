ad9b5dc4944ee1d322d1f53115ea39a71e281231
kishore99 <bhashyamyk@gmail.com>
2022-04-14 22:03:21 +0530

    hsdk

--- 2022/February/Rotate_array.java
+++ 2022/February/Rotate_array.java
@@ -6,7 +6,7 @@ class Solution {
     public void reverse(int[] arr, int left, int right){
         while(left < right)
 		{  // swap till the array is reversed
-      // hello 223666
+      // hello 22366633
             int temp = arr[left];
             arr[left] = arr[right];
             arr[right] = temp;
